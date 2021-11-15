bb63c1f8dfaf3ede86b70d8be4e598dd5944283e
Mert Barut <mbarut@c2s13d65.42wolfsburg.de>
2021-11-15 15:55:24 +0100

    15/11/2021

--- module06/ex00/main.cpp
+++ module06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mbarut <mbarut@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 13:19:31 by mbarut            #+#    #+#             */
-/*   Updated: 2021/11/15 15:09:09 by mbarut           ###   ########.fr       */
+/*   Updated: 2021/11/15 15:53:20 by mbarut           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,60 @@ int main(int argc, char **argv)
 
 		ptr	= argv[1];
 		
+		std::string str = (std::string)((char *)ptr);
+
+		double inf = std::numeric_limits<double>::infinity();
+		float inff = std::numeric_limits<float>::infinity();
+		
+		//std::cout << "TEST: " << str << std::endl;
+		if (!str.compare("-inf"))
+		{
+			std::cout << "char: impossible" << std::endl;
+			std::cout << "int: impossible" << std::endl;
+			std::cout << "float: -" << inff << "f" << std::endl;
+			std::cout << "double: -" << inf << std::endl;
+			return (0);
+		}
+		if (!str.compare("inf") || !str.compare("+inf"))
+		{
+			std::cout << "char: impossible" << std::endl;
+			std::cout << "int: impossible" << std::endl;
+			std::cout << "float: " << inff << "f" << std::endl;
+			std::cout << "double: " << inf << std::endl;
+			return (0);
+		}
+		if (!str.compare("nan"))
+		{
+			std::cout << "char: impossible" << std::endl;
+			std::cout << "int: impossible" << std::endl;
+			std::cout << "float: nanf" << std::endl;
+			std::cout << "double: nan" << std::endl;
+			return (0);
+		}
+		if (!str.compare("-inff"))
+		{
+			std::cout << "char: impossible" << std::endl;
+			std::cout << "int: impossible" << std::endl;
+			std::cout << "float: -" << inff << "f" << std::endl;
+			std::cout << "double: -" << inf << std::endl;
+			return (0);
+		}
+		if (!str.compare("inff") || !str.compare("+inff"))
+		{
+			std::cout << "char: impossible" << std::endl;
+			std::cout << "int: impossible" << std::endl;
+			std::cout << "float: " << inff << "f" << std::endl;
+			std::cout << "double: " << inf << std::endl;
+			return (0);
+		}
+		if (!str.compare("nan"))
+		{
+			std::cout << "char: impossible" << std::endl;
+			std::cout << "int: impossible" << std::endl;
+			std::cout << "float: nanf" << std::endl;
+			std::cout << "double: nan" << std::endl;
+			return (0);
+		}
 		char c = static_cast<char>(((char *)ptr)[0]);
 		if (!isupper(c) && !islower(c))
 			std::cout << "char = Non displayable" << std::endl;
@@ -33,7 +87,11 @@ int main(int argc, char **argv)
 
 		float f = static_cast<float>(std::atof((char *)(ptr)));
 		std::cout << std::fixed << std::setprecision(1);
-		std::cout << "float = " << f << std::endl;
+		std::cout << "float = " << f << "f" << std::endl;
+
+		double d = static_cast<double>((float)std::atof((char *)(ptr)));
+		std::cout << std::fixed << std::setprecision(1);
+		std::cout << "double = " << d << std::endl;
 
 		return (0);
 	}
