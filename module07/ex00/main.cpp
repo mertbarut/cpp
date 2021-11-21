a9ad16c8b3a1d27712d6c5cec77a7f5375a6aa0b
mertbarut <mebarut@icloud.com>
2021-11-21 16:55:15 +0100

    21/11/2021

---
+++ module07/ex00/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbarut <mbarut@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/21 15:55:33 by mbarut            #+#    #+#             */
+/*   Updated: 2021/11/21 16:51:27 by mbarut           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+#include "template.hpp"
+
+int main(void)
+{
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << max( c, d ) << std::endl;
+
+	return 0;
+}
---
+++ module07/ex00/template.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   template.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbarut <mbarut@student.42wolfsburg.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/21 16:02:58 by mbarut            #+#    #+#             */
+/*   Updated: 2021/11/21 16:22:15 by mbarut           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T temp {a};
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T& min(T &a, T &b)
+{
+	return a > b ? b : a;
+}
+
+template <typename T>
+T& max(T &a, T &b)
+{
+	return a > b ? a : b;
+}
